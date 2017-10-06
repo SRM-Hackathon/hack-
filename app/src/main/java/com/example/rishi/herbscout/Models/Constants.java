8cdcbd4b8f0486a55769c59cee8dd1106319e25a
Rishi Sharma <rishisharma7361@gmail.com>
2017-10-06 20:34:42 +0530

    Plant list

---
+++ app/src/main/java/com/example/rishi/herbscout/Models/Constants.java
@@ -0,0 +1,15 @@
+package com.example.rishi.herbscout.Models;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by Rishi on 06/10/17.
+ */
+
+public class Constants {
+
+    final public static String PREFS_NAME="UserLogin";
+    final public static String PREFS_NAME_TOKEN="token";
+    final public static String PREFS_NAME_USERNAME="username";
+    final public static String NULL="NULL";
+}
---
+++ app/src/main/java/com/example/rishi/herbscout/Models/URLS.java
@@ -0,0 +1,14 @@
+package com.example.rishi.herbscout.Models;
+
+/**
+ * Created by Rishi on 06/10/17.
+ */
+
+public class URLS {
+    final public static String BASE_URL="http://10.5.59.123:3000/";
+    final public static String loginURL="auth/login/";
+    final public static String registerURL="auth/register/";
+    final public static String searchImageURL="search/plant_image/";
+    final public static String searchNameURL="search/plant_info";
+    final public static String getSpecificPlantURL="search/get_specific_plant/";
+}
